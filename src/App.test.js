e78004be87be0105cd3d5685fb6e4ae36f5fb188
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-07-09 11:23:38 -0600

    first test passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,67 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import { render } from "@testing-library/react"; // to render components in a virtual DOM
+import App from "./App"; // we need the component we are testing/rendering
+
+// jest global functions to write our tests
+// - test() function // used to run a test
+
+// Jest is going to be our "test runner"
+
+// test('title', cbToRunTheTest)
+test('renders App component without errors', () => {
+//   ^^^^^^^^^^^^^^^^title^^^^^^^^^^^^^^^^,  ^^^^^^^^anonymous callback function
+    // render the App component
+    render(<App/>);
+
+    //tests will fail if an error is "thrown" from any function/component inside the test callback
+    // breakTheTest(true);
+
+})
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////FIRST TEST////
+// function breakTheTest(someValue){
+//     if(someValue === true){
+//         // do nothing!
+//     } else {
+//     throw new Error("this test failed because we threw an error inside 'breakTheTest()'")
+//     }
+// }
